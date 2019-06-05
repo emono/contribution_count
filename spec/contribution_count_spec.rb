7b12f12ceaaf6410d9b41f1480fff4a3b4d5c2e2
emono <ababababa0222@gmail.com>
2019-06-06 00:31:24 +0900

    fix version

--- spec/contribution_count_spec.rb
+++ spec/contribution_count_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe ContributionCount do
   it "has a version number" do
-    expect(ContributionCount::VERSION).to eq "0.1.3"
+    expect(ContributionCount::VERSION).to eq "0.1.4"
   end
 
   context ".date(2019-01-01)" do
